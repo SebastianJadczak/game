1b9ccc837402a146292769ea52ad97d175a9ccdb
sjadczak <sebastian-jadczak@wp.pl>
2021-04-08 17:30:55 +0200

    repair handle register and add alert text

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -5,8 +5,9 @@ class Register extends Component{
         super(props);
       }
       state={
-          credentials:{username:"", password: ""},
-         
+        credentials:{username:"", password: ""},
+        text:"",
+        registerCorrect:false
       }
     
     inputhandle = e =>{
@@ -15,6 +16,17 @@ class Register extends Component{
         this.setState({
             credentials:cred
         })
+    }
+    checkUsername = (data)=>{
+        if(data !== this.state.credentials.username){
+            this.setState({
+                text:"Takie konto już istnieje"
+            })
+        }else{
+            this.setState({
+                registerCorrect:true
+            })
+        }
     }
     register = event=>{
         fetch(`/users`, {
@@ -26,19 +38,26 @@ class Register extends Component{
             data=>data.json()
         )
         .then(
-            data=> console.log(data.token)
+            data=> this.checkUsername(data.username)
         )
         .catch(err=>console.error(err))
     }
+    endRegister=()=>{
+        this.setState({
+            registerCorrect:false
+        })
+        this.props.closeRegister()
+    }
 
     render(){
-        const {credentials}=this.state
+        const {credentials, text, registerCorrect}=this.state
 
     return (
         <div className="register" style={{display:`${this.props.register}`,position:"absolute",top:"0px", left:"0px", width:"98.7vw", minHeight:"198vh",backgroundColor: "rgba(255,255,255, 0.7"}}>
             <div id="register">
                 <h3>Zarejestruj</h3>
-                <div id="form" >
+                {text?<p id="alert">{text}</p>:null}
+                <div id="form">
                     <div id="username"><label>Login:</label><input type="text" name="username" onChange={this.inputhandle} value={credentials.username}/></div>
                     <div id="password"><label>Password:</label><input type="password" name="password" onChange={this.inputhandle} value={credentials.password}/></div>
                     <div id="submit">
@@ -47,6 +66,10 @@ class Register extends Component{
                     </div>
                 </div>
             </div>
+            {registerCorrect?<div id="thanks" >
+                <p>Konto zostało utworzone.</p>
+                <div><button onClick={this.endRegister}>Zamknij</button></div>
+            </div>:null}
         </div>
     )
 }}
